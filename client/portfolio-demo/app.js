73cf15c68b16f85abb8f18180986c7ba7a19f722
Steven C Scott <steven.scott.dev@gmail.com>
2026-07-12 02:51:31 -0400

    Add interactive demo behavior

---
+++ client/portfolio-demo/app.js
@@ -0,0 +1,4 @@
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  alert('AI demo started: collecting customer information and booking appointment.');
+});
